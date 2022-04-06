703a936509324c1f06c5b50e501f3196da4261be
MicheleMallia <mallia.michele@gmail.com>
2022-04-06 19:44:31 +0200

    changed project Name and baseurl

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/lexO-angular'),
+      dir: require('path').join(__dirname, './coverage/LexO-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,16 +32,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lexO-angular'`, () => {
+  it(`should have as title 'LexO-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lexO-angular');
+    expect(app.title).toEqual('LexO-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('lexO-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('LexO-angular app is running!');
   });
 });
