bc2cad05e2d130ad1121a3afc6719c0635e91ee4
MicheleMallia <mallia.michele@gmail.com>
2023-01-25 15:37:22 +0100

    missing file from previous commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ You should have received a copy of the GNU General Public License along with Epi
 */
 
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './services/guard/auth.guard';
 import { HomePageComponent } from './views/home-page/home-page.component';
@@ -25,7 +26,7 @@ const routes: Routes = [
   /* {path: 'home', component: HomePageComponent}, */
   /* {path: 'login', component: LoginPageComponent}, */
   {path: 'lexicon', component: LexiconPageComponent, canActivate: [AuthGuard], data: {roles: ['USER']}},
-  /* {path: 'user', component: ProfilePageComponent, canActivate: [AuthGuard], data: {roles: ['ADMIN']}}, */
+  {path: 'user', component: ProfilePageComponent, canActivate: [AuthGuard], data: {roles: ['ADMIN']}},
   /* {path: 'search', component: SearchPageComponent}, */
   /* {path: '**', component: PageNotFoundComponent} */
   {path: '**', component: LexiconPageComponent}
